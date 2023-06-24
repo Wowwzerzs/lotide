95ab7874037ac1e61e4e7bda3c9491903435d772
Taha Mulla <tahamulla@hotmail.com>
2023-06-23 22:09:21 -0400

    Counts letters in a string

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const letterCount = {};
+
+  // Convert the string to lowercase for case-insensitive counting
+  const lowercaseString = string.toLowerCase();
+
+  // Iterate over each character in the string
+  for (const char of lowercaseString) {
+    // Ignore non-alphabetic characters
+    if (/[a-z]/.test(char)) {
+      // Check if the character already exists in the letterCount object
+      if (letterCount[char]) {
+        letterCount[char] += 1; // Increment the count if it exists by 1
+      } else {
+        letterCount[char] = 1; // Set the count to 1 if it doesn't exist yet
+      }
+    }
+  }
+
+  return letterCount;
+};
+
+console.log(countLetters("LHL")); // Output: { l: 2, h: 1 }
+console.log(countLetters("Hello, world!")); // Output: { h: 1, e: 1, l: 3, o: 2, w: 1, r: 1, d: 1 }
+assertEqual(countLetters("LHL"));
+assertEqual(countLetters("Hello, world!"));

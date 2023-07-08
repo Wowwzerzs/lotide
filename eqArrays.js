bf615e81c9e2ad767a59738cce9f72f7057e6947
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 12:16:51 -0400

    Modularing code

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   // Check if the lengths of the arrays are different
@@ -22,7 +16,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;

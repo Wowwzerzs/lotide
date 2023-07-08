92b77a7d46e33b29196a3151cf01bb1d2f90cf05
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 12:00:04 -0400

    Modularing code

--- tail.js
+++ tail.js
@@ -1,19 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-/*
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual(5, 10);
-*/
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   if (arr.length <= 1) {
@@ -22,13 +7,4 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 2: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;

8727dfc1dbc63cf1f15674f1e30063403d2d0343
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 12:31:07 -0400

    Modularing code

--- middle.js
+++ middle.js
@@ -1,28 +1,10 @@
 // Helper function to compare if two arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 // Helper function to assert if two arrays are equal
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
-const middle = function(arr) {
+const middle = function (arr) {
   const length = arr.length; // Get the length of the array
 
   if (length <= 2) {
@@ -38,10 +20,4 @@ const middle = function(arr) {
   }
 };
 
-// Test cases
-assertArraysEqual(middle([1]), []); // Output: ✅ Assertion Passed: [] === []
-assertArraysEqual(middle([1, 2]), []); // Output: ✅ Assertion Passed: [] === []
-assertArraysEqual(middle([1, 2, 3]), [2]); // Output: ✅ Assertion Passed: [2] === [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Output: ✅ Assertion Passed: [3] === [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Output: ✅ Assertion Passed: [2,3] === [2,3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Output: ✅ Assertion Passed: [3,4] === [3,4]
+module.exports = middle;

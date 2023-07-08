cd83f279e44388d541974cae0cacda27225f007b
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 13:14:59 -0400

    Mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,17 @@
-const assertEqual = require('../assertEqual');
-
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('tail()', function() {
+  it('should not modify the original array', function() {
+    const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
 
-// Test Case 2: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it('should return an array with the elements after the first element', function() {
+    const result = tail(['Hello', 'Lighthouse', 'Labs']);
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual(result[0], 'Lighthouse');
+    assert.strictEqual(result[1], 'Labs');
+  });
+});

8d465a785e2e785a498cd630243b9a91b9ea66e6
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 13:14:53 -0400

    Mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,23 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 
-// Test cases
-assertArraysEqual(middle([1]), []); // Output: ✅ Assertion Passed: [] === []
-assertArraysEqual(middle([1, 2]), []); // Output: ✅ Assertion Passed: [] === []
-assertArraysEqual(middle([1, 2, 3]), [2]); // Output: ✅ Assertion Passed: [2] === [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Output: ✅ Assertion Passed: [3] === [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Output: ✅ Assertion Passed: [2,3] === [2,3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Output: ✅ Assertion Passed: [3,4] === [3,4]
+describe('middle()', function() {
+  it('should return an empty array for an input array of length 1', function() {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('should return an empty array for an input array of length 2', function() {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('should return the middle element for an input array of odd length', function() {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('should return the two middle elements for an input array of even length', function() {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});

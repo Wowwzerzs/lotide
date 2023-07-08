7fb254ac8f0c05032ba4981f0cd16bfdb156bc65
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 12:30:46 -0400

    eslint code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
   } else {

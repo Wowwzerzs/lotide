c386b9f6c7a3b47436f298229386bf6e9b0b4efa
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 13:14:37 -0400

    Mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,12 @@
-const assertEqual = require('../assertEqual');
-
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+describe('eqArrays()', function() {
+  it('should return true for equal arrays', function() {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it('should return false for non-equal arrays', function() {
+    assert.isFalse(eqArrays([1, 2, 3], [3, 2, 1]));
+  });
+});

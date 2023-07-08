6218c16e126d84fa07fb6014df3399cb5c1a899d
Taha Mulla <tahamulla@hotmail.com>
2023-07-08 13:14:28 -0400

    Mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,23 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual(5, 10);
+describe('assertEqual()', function() {
+  it('should not throw an error for equal values', function() {
+    assert.doesNotThrow(function() {
+      assertEqual("Lighthouse Labs", "Bootcamp");
+    });
+    assert.doesNotThrow(function() {
+      assertEqual(1, 1);
+    });
+    assert.doesNotThrow(function() {
+      assertEqual("Hello", "Hello");
+    });
+  });
+
+  it('should throw an error for non-equal values', function() {
+    assert.doesNotThrow(function() {
+      assertEqual(5, 10);
+    });
+  });
+});
+
